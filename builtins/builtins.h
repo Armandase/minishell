944c01e3d0642cd7911385d0096d896556573c4c
0x35c <ulayus@1E5.42angouleme.fr>
2023-01-12 15:07:11 +0100

    Modif on export and env to parse args

--- builtins/builtins.h
+++ builtins/builtins.h
@@ -5,10 +5,10 @@
 # include "struct.h"
 # include "unistd.h"
 
+t_env_list	*create_env_list(char **envp);
 void		main_env(t_env_list *list_var);
 void		main_pwd(void);
-t_env_list	*main_export(char *name, char *value, t_env_list *list_var);
-t_env_list	*main_unset(char *arg_name, t_env_list *list_var);
-t_env_list	*create_env_list(char **envp);
+void		*main_export(char *name, char *value, t_env_list **list_var);
+void		*main_unset(char *arg_name, t_env_list **list_var);
 
 #endif
--- builtins/export.c
+++ builtins/export.c
@@ -1,21 +1,57 @@
 #include "builtins.h"
 
-t_env_list	*main_export(char *name, char *value, t_env_list *list_var)
+char	*export_name(char *str)
 {
-	t_env_list	*tmp;
-	t_env_list	*new_var;
+	char	*name;
+	int		i;
 
-	tmp = list_var;
-	new_var = ft_calloc(1, sizeof(t_env_list));
-	if (new_var == NULL)
-		return (NULL); //A faire plus propre avec un ft_exit_error
-	new_var->name = name;
-	new_var->value = value;
-	new_var->next = NULL;
-	if (list_var == NULL)
-		return (new_var);
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new_var;
-	return (list_var);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '=')
+			break ;
+		i++;
+	}
+	name = ft_calloc(i + 1, 1);
+	if (name == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		name[i] = str[i];
+		if (name[i] == '=')
+			break ;
+		i++;
+	}
+	return (name);
+}
+
+void	*main_export(char **args, t_env_list **list_var)
+{
+	t_env_list	*new_var;
+	char		*name;
+	char		*value;
+	int			i;
+
+	i = 1;
+	while (args[i])
+	{
+		if (ft_strchr(args[i], '='))
+		{
+			name = NULL;
+			name = export_name(args[1]);
+			new_var = ft_calloc(1, sizeof(t_env_list));
+			if (new_var == NULL)
+				return (NULL); //A faire plus propre avec un ft_exit_error
+			new_var->args = name;
+			new_var->value = value;
+			new_var->next = NULL;
+			if (list_var == NULL)
+				list_var = &new_var;
+			while ((*list_var)->next)
+				(*list_var) = (*list_var)->next;
+			(*list_var)->next = new_var;
+		}
+		i++;
+	}
 }
