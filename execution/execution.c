c9eb337210b76c61cf04845133010b5e8aa411f5
Armand Damiens <adamiens@1E7.42angouleme.fr>
2023-01-12 15:14:20 +0100

    exec update

--- execution/execution.c
+++ execution/execution.c
@@ -1,6 +1,6 @@
 #include "execution.h"
 
-void	execution(t_cmd *cmd, char **envp)
+void	execution(t_cmd *cmd, char **envp, t_env_list *list_var)
 {
 	int	i;
 
@@ -8,7 +8,7 @@ void	execution(t_cmd *cmd, char **envp)
 	while (cmd[i].cmd != NULL)
 	{
 		if (cmd[i].token == PIPE || cmd[i].token == 0)
-			exec_cmd(cmd, i, envp);
+			exec_cmd(cmd, i, envp, list_var);
 		i++;
 	}
 }
