386008e4bca29da1ae3fed55f27125b968829e07
0x35c <unai64@orange.fr>
2023-01-25 15:26:48 +0100

    Norm on builtins done

--- builtins/export.c
+++ builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: ulayus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 14:14:37 by ulayus            #+#    #+#             */
-/*   Updated: 2023/01/25 13:51:55 by ulayus           ###   ########.fr       */
+/*   Updated: 2023/01/25 15:15:44 by ulayus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,42 +64,55 @@ static int	replace_value(char *name, char *value, t_env_list **list_var,
 	return (0);
 }
 
-int	main_export(char **args, t_env_list **list_var)
+static int	add_env_var(char *line, t_env_list **list_var, char *name,
+		char *value)
 {
 	t_env_list	*new_var;
-	char		*name;
-	char		*value;
-	int			i;
+
+	while (line && ft_strcmp(line, "export"))
+	{
+		name = export_name(line);
+		value = export_value(line);
+		if (check_format(line, name))
+			return (1);
+		if (search_var(name, list_var) == true && value && *value)
+		{
+			search_replace_var(name, value, list_var);
+			free(name);
+			return (0);
+		}
+		new_var = ft_calloc(1, sizeof(t_env_list));
+		if (new_var == NULL)
+			return (12);
+		if (check_assign_symbol(line, name, list_var, new_var) == BREAK)
+			return (0);
+		new_var->value = value;
+		new_var->next = NULL;
+		if (replace_value(name, value, list_var, new_var) == BREAK)
+			return (0);
+	}
+	return (0);
+}
+
+int	main_export(char **args, t_env_list **list_var)
+{
+	char	*name;
+	char	*value;
+	int		check;
+	int		i;
 
 	i = 0;
+	name = NULL;
+	value = NULL;
+	if (ft_strlen_2d((const char **)args) == 1
+		&& ft_strcmp(args[i], "export") == 0)
+		print_args_ascii(*list_var, env_list_size(*list_var));
 	while (args[i])
 	{
-		while (ft_strcmp(args[i], "export"))
-		{
-			name = export_name(args[i]);
-			value = export_value(args[i]);
-			if (check_format(args[i], name))
-				return (1);
-			if (search_var(name, list_var) == true && value && *value)
-			{
-				search_replace_var(name, value, list_var);
-				free(name);
-				break ;
-			}
-			new_var = ft_calloc(1, sizeof(t_env_list));
-			if (new_var == NULL)
-				return (12);
-			if (check_assign_symbol(args[i], name, list_var, new_var) == BREAK)
-				break ;
-			new_var->value = value;
-			new_var->next = NULL;
-			if (replace_value(name, value, list_var, new_var) == BREAK)
-				break ;
-			break ;
-		}
+		check = add_env_var(args[i], list_var, name, value);
+		if (check != 0)
+			return (check);
 		i++;
 	}
-	if (i == 0 && ft_strcmp(args[i], "export") == 0)
-		print_args_ascii(*list_var, env_list_size(*list_var));
 	return (0);
 }
