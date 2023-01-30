1436094bed4ef4ddcb60d773bf922d844a66e696
0x35c <unai64@orange.fr>
2023-01-30 13:08:29 +0100

    "

--- execution/signals_handlers.c
+++ execution/signals_handlers.c
@@ -6,7 +6,7 @@
 /*   By: adamiens <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:58:22 by adamiens          #+#    #+#             */
-/*   Updated: 2023/01/30 12:53:47 by ulayus           ###   ########.fr       */
+/*   Updated: 2023/01/30 12:57:07 by ulayus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,16 +39,19 @@ void	handle_sigint(int signum)
 		rl_on_new_line();
 		rl_redisplay();
 	}
-	else if (g_sh_state.state == HERE_DOC)
-		g_sh_state.check_signal = true;
 }
 
 void	handle_sigint_exec(int signum)
 {
 	(void)signum;
-	rl_replace_line("", 0);
-	ft_printf("\n");
-	rl_on_new_line();
+	if (g_sh_state.state == HERE_DOC)
+		g_sh_state.check_signal = true;
+	else
+	{
+		rl_replace_line("", 0);
+		ft_printf("\n");
+		rl_on_new_line();
+	}
 }
 
 void	handle_sigquit(int signum)
